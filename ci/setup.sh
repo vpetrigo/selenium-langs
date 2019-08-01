a8e3112e3537e6cd5c0c84997a71d9f080681169
Vladimir Petrigo <taenaru@gmail.com>
2019-08-01 21:55:08 +0300

    ci: Make chromedriver executable rights

--- ci/setup.sh
+++ ci/setup.sh
@@ -4,4 +4,5 @@ set -euxo pipefail
 
 curl "https://chromedriver.storage.googleapis.com/76.0.3809.68/chromedriver_linux64.zip" -o chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+chmod +x ./chromedriver
 export PATH=`pwd`:$PATH
