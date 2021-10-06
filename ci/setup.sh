e49a27cb79b309aa404844872748668d4dd32893
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2021-10-06 16:46:05 +0300

    Update setup.sh

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-curl "https://chromedriver.storage.googleapis.com/76.0.3809.68/chromedriver_linux64.zip" -o chromedriver_linux64.zip
+curl "https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip" -o chromedriver_linux64.zip
 unzip chromedriver_linux64.zip -d .
 chmod +x ./chromedriver
 ./chromedriver --version
